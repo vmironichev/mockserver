82282f8f0ed85d550a74d60df89d4df3e6435ea5
Valeriy Mironichev <vmironichev@chegg.com>
2016-07-01 16:03:42 +0300

    Added web hook field value policy for date fields and for pass through attributes. Incremented Java compiler version to 1.7

--- mockserver-core/src/main/java/org/mockserver/mock/action/HttpWebHookActionHandler.java
+++ mockserver-core/src/main/java/org/mockserver/mock/action/HttpWebHookActionHandler.java
@@ -1,10 +1,7 @@
 package org.mockserver.mock.action;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.UUID;
 
-import org.apache.commons.lang.StringEscapeUtils;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
 import org.mockserver.model.HttpWebHook;
@@ -21,94 +18,25 @@ public class HttpWebHookActionHandler extends HttpResponseActionHandler {
 
         HttpWebHookConfig webHookConfig = httpWebHook.getHttpWebHookConfig();
         if (webHookConfig != null) {
-
+            String requestBody = httpRequest.getBodyAsString();
             List<HttpWebHookRequest> requests = webHookConfig.getEndpoints();
             List<ResponsePayloadFieldValuePolicy> valuePolicies = webHookConfig.getResponsePayloadFieldValuePolicies();
-            if (valuePolicies != null) {
-                applyFieldValuePolicies(httpRequest, valuePolicies, requests);
-            }
-
-            for (HttpWebHookRequest request : requests) {
-                request.submit();
+            for (HttpWebHookRequest webHookRequest : requests) {
+                webHookRequest.setBody(webHookRequest.getPayload());
+                applyFieldValuePolicies(requestBody, valuePolicies, webHookRequest);
+                webHookRequest.submit();
             }
         }
         return httpWebHook.getHttpResponse().shallowClone();
     }
 
-    private void applyFieldValuePolicies(HttpRequest httpRequest, List<ResponsePayloadFieldValuePolicy> valuePolicies,
-            List<HttpWebHookRequest> webHookRequests) {
-
-        String requestBody = httpRequest.getBodyAsString();
-
-        for (HttpWebHookRequest request : webHookRequests) {
-
-            String requestPayload = request.getPayload();
-            String body = requestPayload;
-
+    private void applyFieldValuePolicies(String requestBody, List<ResponsePayloadFieldValuePolicy> valuePolicies,
+            HttpWebHookRequest webHookRequest) {
+        if (valuePolicies != null) {
             for (ResponsePayloadFieldValuePolicy policy : valuePolicies) {
-                String strategy = policy.getPopulateStrategy();
-                String fieldName = policy.getFieldName();
-                String fieldType = policy.getFieldType();
-                String escapedFieldName = "\\$\\{" + fieldName + "\\}";
-                if (requestPayload.contains(fieldName)) {
-                    switch (strategy) {
-                    case "distribute_from_request":
-                        if ("decimal".equals(fieldType)) {
-                            String fieldValue = getFieldValue(requestBody, fieldName);
-                            if (fieldValue != null) {
-                                BigDecimal decimalValue = null;
-                                try {
-                                    decimalValue = new BigDecimal(fieldValue);
-                                    String unescapedFieldName = StringEscapeUtils.unescapeJava(escapedFieldName);
-                                    int count = countFieldOccurences(unescapedFieldName, requestPayload);
-                                    if (count > 0) {
-                                        String newValue = decimalValue.divide(new BigDecimal(count)).toString();
-                                        while (body.contains(StringEscapeUtils.unescapeJava(escapedFieldName))) {
-                                            body = body.replaceFirst(escapedFieldName, newValue);
-                                        }
-                                        request.setBody(body);
-                                    }
-                                } catch (NumberFormatException e) {
-                                }
-                            }
-                        }
-                        break;
-                    case "auto":
-                        if ("uuid4".equals(fieldType)) {
-                            while (body.contains(StringEscapeUtils.unescapeJava(escapedFieldName))) {
-                                body = body.replaceFirst(escapedFieldName, UUID.randomUUID().toString().replace("-", ""));
-                            }
-                            request.setBody(body);
-                        }
-                        break;
-                    }
-                }
+                policy.apply(requestBody, webHookRequest);
             }
         }
-    }
-
-    private String getFieldValue(String requestBody, String fieldName) {
-        String fieldValue = null;
-        try {
-            String fieldValueBegining = requestBody.substring(requestBody.lastIndexOf(fieldName));
-            fieldValue = fieldValueBegining.substring(fieldValueBegining.indexOf(":") + 1,
-                    fieldValueBegining.indexOf(","));
-        } catch (Exception e) {
-        }
-        return fieldValue == null ? null : fieldValue.trim();
-    }
-
-    private int countFieldOccurences(String toFind, String inString) {
-        int count = 0;
-        int lastIndex = 0;
-        while (lastIndex != -1) {
-            lastIndex = inString.indexOf(toFind, lastIndex);
-            if (lastIndex != -1) {
-                count++;
-                lastIndex += toFind.length();
-            }
-        }
-        return count;
     }
 
 }
--- mockserver-core/src/main/java/org/mockserver/model/ResponsePayloadFieldValuePolicy.java
+++ mockserver-core/src/main/java/org/mockserver/model/ResponsePayloadFieldValuePolicy.java
@@ -1,12 +1,21 @@
 package org.mockserver.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
 /**
  * author Valeriy Mironichev
  */
-public class ResponsePayloadFieldValuePolicy extends ObjectWithReflectiveEqualsHashCodeToString{
+public class ResponsePayloadFieldValuePolicy extends ObjectWithReflectiveEqualsHashCodeToString {
     private String fieldName;
     private String fieldType;
     private String populateStrategy;
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public ResponsePayloadFieldValuePolicy() {
     }
@@ -14,6 +23,7 @@ public class ResponsePayloadFieldValuePolicy extends ObjectWithReflectiveEqualsH
     public String getFieldName() {
         return fieldName;
     }
+
     public void setFieldName(String fieldName) {
         this.fieldName = fieldName;
     }
@@ -32,6 +42,98 @@ public class ResponsePayloadFieldValuePolicy extends ObjectWithReflectiveEqualsH
 
     public void setPopulateStrategy(String populateStrategy) {
         this.populateStrategy = populateStrategy;
+    }
+
+    public void apply(String requestBody, HttpWebHookRequest webHook) {
+
+        String webHookPayloadTemplate = webHook.getPayload();
+        String body = webHook.getResponseBody();
+
+        String escapedFieldName = "\\$\\{" + fieldName + "\\}";
+        String value = null;
+        // TODO(vmironichev): refactor to strategy classes
+        if (webHookPayloadTemplate.contains(fieldName)) {
+            switch (populateStrategy) {
+            case "pass_from_request":
+                value = getJsonAttributeValue(requestBody, fieldName);
+                while (body.contains(StringEscapeUtils.unescapeJava(escapedFieldName))) {
+                    body = body.replaceFirst(escapedFieldName, value);
+                    webHook.setBody(body);
+                }
+                break;
+            case "distribute_from_request":
+                if ("decimal".equals(fieldType)) {
+                    String fieldValue = getJsonAttributeValue(requestBody, fieldName);
+                    if (isNumeric(fieldValue)) {
+                        int count = countFieldOccurences(StringEscapeUtils.unescapeJava(escapedFieldName), webHookPayloadTemplate);
+                        if (count > 0) {
+                            body = populateDecimalValue(body, escapedFieldName, fieldValue, count);
+                            webHook.setBody(body);
+                        }
+                    }
+                }
+                break;
+            case "auto":
+                while (body.contains(StringEscapeUtils.unescapeJava(escapedFieldName))) {
+                    value = generateFieldValue(fieldType, value);
+                    body = body.replaceFirst(escapedFieldName, value);
+                }
+                webHook.setBody(body);
+                break;
+            }
+        }
+    }
+
+    private boolean isNumeric(String fieldValue) {
+        return fieldValue.matches("-?\\d+(.\\d+)?");
+    }
+
+    private String populateDecimalValue(String body, String escapedFieldName, String fieldValue, int count) {
+
+        BigDecimal newValue = new BigDecimal(fieldValue).divide(new BigDecimal(count)).setScale(2,
+                RoundingMode.HALF_UP);
+        while (body.contains(StringEscapeUtils.unescapeJava(escapedFieldName))) {
+            body = body.replaceFirst(escapedFieldName, newValue.toString());
+        }
+        return body;
+    }
+
+    private String generateFieldValue(String fieldType, String value) {
+        switch (fieldType) {
+        case "uuid4":
+            value = UUID.randomUUID().toString().replace("-", "");
+            break;
+        case "date":
+            value = dateFormat.format(new Date());
+            break;
+        default:
+            break;
+        }
+        return value;
+    }
+
+    private String getJsonAttributeValue(String requestBody, String fieldName) {
+        String fieldValue = null;
+        try {
+            String fieldValueBegining = requestBody.substring(requestBody.lastIndexOf(fieldName));
+            fieldValue = fieldValueBegining.substring(fieldValueBegining.indexOf(":") + 1,
+                    fieldValueBegining.indexOf(","));
+        } catch (Exception e) {
+        }
+        return fieldValue == null ? null : fieldValue.replace("\"", "").trim();
+    }
+
+    private int countFieldOccurences(String toFind, String inString) {
+        int count = 0;
+        int lastIndex = 0;
+        while (lastIndex != -1) {
+            lastIndex = inString.indexOf(toFind, lastIndex);
+            if (lastIndex != -1) {
+                count++;
+                lastIndex += toFind.length();
+            }
+        }
+        return count;
     }
 
 }
